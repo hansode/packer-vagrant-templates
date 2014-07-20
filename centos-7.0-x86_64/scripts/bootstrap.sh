36e817eda61883f6911f3ab2b291b22813aa517d
Masahito Yoshida <masahito@axsh.net>
2014-07-20 10:18:20 +0900

    reorganize scripts.

---
+++ centos-7.0-x86_64/scripts/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# requires:
+#  bash
+#
+set -e
+set -o pipefail
+set -x
+
+function yum() {
+  $(type -P yum) --disablerepo=updates "${@}"
+}
+
+# Add installation packages ...
+addpkgs="
+ ntp ntpdate
+ man
+ sudo rsync git make
+ vim-minimal screen
+ nmap lsof strace tcpdump traceroute telnet ltrace bind-utils sysstat nc
+ wireshark
+ zip
+"
+
+if [[ -n "$(echo ${addpkgs})" ]]; then
+  yum install -y ${addpkgs}
+fi
--- centos-7.0-x86_64/scripts/sshd_config.sh
+++ centos-7.0-x86_64/scripts/sshd_config.sh
@@ -7,29 +7,6 @@ set -e
 set -o pipefail
 set -x
 
-#> bootstrap.sh
-
-function yum() {
-  $(type -P yum) --disablerepo=updates "${@}"
-}
-
-# Add installation packages ...
-addpkgs="
- ntp ntpdate
- man
- sudo rsync git make
- vim-minimal screen
- nmap lsof strace tcpdump traceroute telnet ltrace bind-utils sysstat nc
- wireshark
- zip
-"
-
-if [[ -n "$(echo ${addpkgs})" ]]; then
-  yum install -y ${addpkgs}
-fi
-
-#> sshd_config.sh
-
 function config_sshd_config() {
   local sshd_config_path=$1 keyword=$2 value=$3
   [[ -a "${sshd_config_path}" ]] || { echo "[ERROR] file not found: ${sshd_config_path} (${BASH_SOURCE[0]##*/}:${LINENO})" >&2; return 1; }
@@ -97,53 +74,4 @@ function config_sshd_config() {
 	UseDNS        no
 	EOS
 	)
-}
-
-#> vagrant.guest.account.sh
-
-{
-  user_name=vagrant
-  user_group=${user_name}
-  user_home=/home/${user_name}
-
-  getent group  ${user_group} >/dev/null || groupadd    ${user_group}
-  getent passwd ${user_name}  >/dev/null || useradd  -g ${user_group} -d ${user_home} -s /bin/bash -m ${user_name}
-
-  egrep -q ^umask ${user_home}/.bashrc || {
-    echo umask 022 >> ${user_home}/.bashrc
-  }
-
-  user_ssh_dir=${user_home}/.ssh
-  authorized_keys_path=${user_ssh_dir}/authorized_keys
-
-  [[ -d "${user_ssh_dir}" ]] || mkdir -m 0700 ${user_ssh_dir}
-  # make sure to directory attribute is 0700
-  chmod 0700 ${user_ssh_dir}
-
-  until curl -fsSkL https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub >> ${authorized_keys_path}; do
-    sleep 1
-  done
-
-  echo         root:${user_name} | chpasswd
-  echo ${user_name}:${user_name} | chpasswd
-
-  chown -R ${user_group}:${user_name} ${user_ssh_dir}
-
-  sed -i "s/^\(^Defaults\s*requiretty\).*/# \1/" /etc/sudoers
-  egrep ^${user_name} -w /etc/sudoers || { echo "${user_name} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers; }
-}
-
-#> virtualbox.guest.additions.sh
-
-{
-  iso_path=/home/vagrant/VBoxGuestAdditions.iso
-  mnt_path=/mnt
-
-  mount -o loop ${iso_path} ${mnt_path}
-  yum install --disablerepo=updates -y make kernel-devel gcc perl bzip2
-
-  ${mnt_path}/VBoxLinuxAdditions.run --nox11 || :
-
-  umount ${mnt_path}
-  rm ${iso_path}
 }
