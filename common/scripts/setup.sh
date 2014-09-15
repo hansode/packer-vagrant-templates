f812b2380ecc80b3621adb64a7a4774cda6ddc94
Masahito Yoshida <masahito@axsh.net>
2014-09-15 16:28:24 +0900

    add "baseurl" variable.

--- common/scripts/setup.sh
+++ common/scripts/setup.sh
@@ -12,16 +12,18 @@ mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.saved
 releasever=$(< /etc/yum/vars/releasever)
 majorver=${releasever%%.*}
 
+baseurl=http://ftp.riken.jp/Linux/centos
+
 cat <<-REPO > /etc/yum.repos.d/CentOS-Base.repo
 	[base]
 	name=CentOS-\$releasever - Base
-	baseurl=http://ftp.riken.jp/Linux/centos/\$releasever/os/\$basearch/
+	baseurl=${baseurl}/\$releasever/os/\$basearch/
 	gpgcheck=1
 	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${majorver}
 
 	[updates]
 	name=CentOS-\$releasever - Updates
-	baseurl=http://ftp.riken.jp/Linux/centos/\$releasever/updates/\$basearch/
+	baseurl=${baseurl}/\$releasever/updates/\$basearch/
 	gpgcheck=1
 	gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-${majorver}
 	REPO
