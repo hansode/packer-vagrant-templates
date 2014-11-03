1afd5e59b5e24c9911a8f7fda54112eee7d56682
Masahito Yoshida <masahito@axsh.net>
2014-11-03 19:52:26 +0900

    remove rpmfusion-free-release after installing VirtualBox-guest.

--- common/scripts/virtualbox.guest.additions.sh
+++ common/scripts/virtualbox.guest.additions.sh
@@ -9,19 +9,22 @@ set -x
 
 {
   yum install --disablerepo=updates -y make kernel-devel gcc perl bzip2
+  iso_path=/home/vagrant/VBoxGuestAdditions.iso
+  mnt_path=/mnt
 }
 
 if [[ -n "$(rpm -E %fedora)" ]]; then
   yum localinstall --nogpgcheck -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
   yum install --disablerepo=updates --disablerepo=rpmfusion-free-updates -y VirtualBox-guest
+  yum remove -y rpmfusion-free-release-$(rpm -E %fedora)
 else
-  iso_path=/home/vagrant/VBoxGuestAdditions.iso
-  mnt_path=/mnt
-
   mount -o loop ${iso_path} ${mnt_path}
 
   ${mnt_path}/VBoxLinuxAdditions.run --nox11 || :
 
   umount ${mnt_path}
-  rm ${iso_path}
 fi
+
+{
+  rm ${iso_path}
+}
