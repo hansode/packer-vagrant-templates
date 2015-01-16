0ca10350209252e8eaa44ed1414ee4c8538e8f90
Masahito Yoshida <masahito@axsh.net>
2015-01-16 14:48:26 +0900

    fix distro handling.

--- common/scripts/virtualbox.guest.additions.sh
+++ common/scripts/virtualbox.guest.additions.sh
@@ -13,17 +13,20 @@ set -x
   mnt_path=/mnt
 }
 
-if [[ -n "$(rpm -E %fedora)" ]]; then
-  yum localinstall --nogpgcheck -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-  yum install --disablerepo=updates --disablerepo=rpmfusion-free-updates -y VirtualBox-guest
-  yum remove -y rpmfusion-free-release-$(rpm -E %fedora)
-else
-  mount -o loop ${iso_path} ${mnt_path}
+rpm -E %fedora
 
-  ${mnt_path}/VBoxLinuxAdditions.run --nox11 || :
-
-  umount ${mnt_path}
-fi
+case "$(rpm -E %fedora)" in
+  %fedora) # centos
+    mount -o loop ${iso_path} ${mnt_path}
+    ${mnt_path}/VBoxLinuxAdditions.run --nox11 || :
+    umount ${mnt_path}
+    ;;
+  [0-9]*) # maybe fedora
+    yum localinstall --nogpgcheck -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+    yum install --disablerepo=updates --disablerepo=rpmfusion-free-updates -y VirtualBox-guest
+    yum remove -y rpmfusion-free-release-$(rpm -E %fedora)
+    ;;
+esac
 
 {
   rm ${iso_path}
